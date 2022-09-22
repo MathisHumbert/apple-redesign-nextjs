5bffc72596dbefb1d8953ae54e64e69ef074f0e5
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-09-22 10:53:40 +0200

    set up apple redesign project

---
+++ components/Landing.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Landing() {
+  return (
+    <section>
+      L
+      <div>
+        <h1>
+          <span>Powered</span>
+          <span className='block'></span>
+          <span className='block'></span>
+        </h1>
+      </div>
+    </section>
+  );
+}
+
+export default Landing;
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+import Header from '../components/Header';
+import Landing from '../components/Landing';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Apple Redesign</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <Header />
+      <main>
+        <Landing />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
